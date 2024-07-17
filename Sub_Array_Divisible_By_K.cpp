957e2f19d30ac05c82b69b807f3ad0c622a510ab
ShivamAnand26 <anandshivi26@gmail.com>
2024-07-17 12:43:51 +0530

    Day 2

--- Sub_Array_Divisible_By_K.cpp
+++ Sub_Array_Divisible_By_K.cpp
@@ -1,22 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// int subArrayDivByK(vector<int> v, int k)
+// {
+//     int cnt = 0;
+//     for (int i = 0; i < v.size(); i++)
+//     {
+//         int sum = 0;
+//         for (int j = i; j < v.size(); j++)
+//         {
+//             sum += v[j];
+//             if (sum % k == 0)
+//             {
+//                 cnt++;
+//             }
+//         }
+//     }
+//     return cnt;
+// }
+
+// Method 2
 int subArrayDivByK(vector<int> v, int k)
 {
+    int sum = 0;
+    int n = v.size();
     int cnt = 0;
-    for (int i = 0; i < v.size(); i++)
+
+    unordered_map<int, int> m;
+    m[0] = 1;
+
+    for (int i : v)
     {
-        int sum = 0;
-        for (int j = i; j < v.size(); j++)
+        sum += i;
+        int mod = sum % k;
+        if (mod < 0)
         {
-            sum += v[j];
-            if (sum % k == 0)
-            {
-                cnt++;
-            }
+            mod += k;
         }
+        if (m.find(mod) != m.end())
+        {
+            cnt += m[mod];
+        }
+        m[mod]++;
     }
-    return cnt++;
+    return cnt;
 }
 
 int main()
